bc60115cd9bacf5ce6a80b2c7cd89ae2def7b7b5
Saipavan Mittapalli <mittapallisaipavan@gmail.com>
2026-04-14 18:31:53 +0530

    project card updated

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -37,7 +37,7 @@ const Work = () => {
                 <span className="project-category">{project.category}</span>
                 <h3 className="project-title">{project.title}</h3>
                 <p className="project-desc">{project.description}</p>
-                <a href={project.link} className="project-link" >View Project →</a>
+                <a href={project.link} target='_blank' className="project-link" >View Project →</a>
               </div>
             </div>
           ))}
